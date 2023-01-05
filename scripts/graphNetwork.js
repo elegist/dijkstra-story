1455516926c237b3ccefc6060a4ce791bfd8ed1e
Robin Patzak <robinpatzak@gmail.com>
2023-01-05 14:55:29 +0100

    added svg resources, build basic frontend, added first animations

---
+++ scripts/graphNetwork.js
@@ -0,0 +1,36 @@
+const nodes = document.querySelectorAll(".node");
+
+gsap.set(".path", { opacity: 0 });
+
+let appearAnimation = gsap.timeline({
+    defaults: { duration: .85, ease: Back.easeOut.config(2), opacity: 0 },
+});
+
+appearAnimation
+    .from(".sticky-note", {
+        scale: 0.2,
+        x: -20,
+        y: 20,
+        transformOrigin: "center",
+        rotation: 30,
+        stagger: {
+            each: .1,
+            from: "random",
+            grid: "auto",
+            ease: "power3.inOut"
+        }
+    })
+    .from(".pin", {
+        scale: 1.4,
+        transformOrigin: "center",
+        y: -20,
+        ease: "expo.out",
+        stagger: {
+            each: .1,
+            from: "random",
+            grid: "auto",
+            ease: "power3.inOut"
+        }
+    });
+
+
