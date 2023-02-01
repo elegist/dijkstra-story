72c3f42e33aaa310608ac6b7acf350aa7ddabf9e
Robin Patzak <robin.patzak@proton.me>
2023-02-01 20:39:07 +0100

    moved dijkstra function into its own file

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+import dijkstra from "./dijkstra.js";
+
 let restartButton = $("#restartButton");
 
 restartButton.on("click", () => {
@@ -354,7 +356,7 @@ let graph = {
 };
 
 const algorithmStepDuration = 1.4;
-let storyFinished = false;
+
 /**
  * Method that adds all the nodes with the corrosponding links to the graph object
  * Automatically calculates logical weights between story nodes dependend on whether or not
@@ -826,113 +828,6 @@ const showAllPaths = (resultPath) => {
         });
     });
 };
-
-/**
- * Implementation of the dijkstra algorithm. Finds the shortest path between two points
- *
- * @param {Object{Array, Array}} graph Represents the network made out of nodes. Graph should include all of the edge informations.
- * @param {String} start Starting point of the algorithm.
- * @param {String} end Ending point of the algorithm.
- */
-function dijkstra(graph, startNodeId, endNodeId) {
-    // Initialize letiables
-    let unvisitedNodes = new PriorityQueue();
-    let distance = {};
-    let previous = {};
-
-    // Set initial conditions
-    graph.nodes.forEach(function (d) {
-        distance[d.id] = Infinity;
-        previous[d.id] = null;
-        unvisitedNodes.enqueue(d.id, distance[d.id]);
-    });
-    distance[startNodeId] = 0;
-    unvisitedNodes.updatePriority(startNodeId, 0);
-
-    let traversalArray = [];
-
-    // Main loop
-    while (unvisitedNodes.length > 0) {
-        // Find the unvisited node with the smallest distance
-        let currentNodeId = unvisitedNodes.dequeue();
-        traversalArray.push(currentNodeId);
-
-        // Stop if we've reached the end node
-        if (currentNodeId === endNodeId) {
-            break;
-        }
-
-        // Update the distance of each neighbor
-        graph.links
-            .filter(function (d) {
-                return (
-                    d.source.id === currentNodeId ||
-                    d.target.id === currentNodeId
-                );
-            })
-            .forEach(function (link) {
-                let neighborId =
-                    link.target.id === currentNodeId
-                        ? link.source.id
-                        : link.target.id;
-                let newDistance = distance[currentNodeId] + link.weight;
-                if (newDistance < distance[neighborId]) {
-                    distance[neighborId] = newDistance;
-                    previous[neighborId] = currentNodeId;
-                    unvisitedNodes.updatePriority(neighborId, newDistance);
-                }
-            });
-    }
-
-    // Build the path
-    let path = [];
-    let currentNode = endNodeId;
-    while (currentNode !== null) {
-        path.unshift(currentNode);
-        currentNode = previous[currentNode];
-    }
-
-    return {
-        path: path,
-        traversalArray,
-        distance: distance[endNodeId],
-    };
-}
-
-//PriorityQueue
-class PriorityQueue {
-    constructor() {
-        this.queue = [];
-        this.priorities = {};
-    }
-
-    enqueue(value, priority) {
-        this.queue.push(value);
-        this.priorities[value] = priority;
-    }
-
-    dequeue() {
-        let highestPriority = Infinity;
-        let highestPriorityIndex = null;
-
-        for (let i = 0; i < this.queue.length; i++) {
-            if (this.priorities[this.queue[i]] < highestPriority) {
-                highestPriority = this.priorities[this.queue[i]];
-                highestPriorityIndex = i;
-            }
-        }
-
-        let dequeued = this.queue.splice(highestPriorityIndex, 1);
-        return dequeued[0];
-    }
-
-    updatePriority(value, priority) {
-        this.priorities[value] = priority;
-    }
-    get length() {
-        return this.queue.length;
-    }
-}
 
 /******************
  *
--- scripts/dijkstra.js
+++ scripts/dijkstra.js
@@ -1,119 +1,108 @@
-//import PriorityQueue from "./priorityQueue.js";
-//import nodeRelations from "../resources/nodeRelations.json" assert { type: "json" };
-
-//console.clear();
-
-let network = document.getElementById("GraphNetwork");
-
-let graph = new Map();
-
-nodeRelations.forEach((relation) => {
-    let neighborMap = new Map();
-    relation.neighbors.forEach((neighbor) => {
-        neighborMap.set(neighbor.id, neighbor.weight);
-        graph.set(relation.id, neighborMap);
-    });
-});
-
-graph.forEach((neighbors, node) => {
-    neighbors.forEach((weight, neighbor) => {
-        let startNodePosX = document.getElementById(node).getAttribute("cx");
-        let startNodePosY = document.getElementById(node).getAttribute("cy");
-
-        let endNodePosX = document.getElementById(neighbor).getAttribute("cx");
-        let endNodePosY = document.getElementById(neighbor).getAttribute("cy");
-
-        let dPath = `M ${startNodePosX} ${startNodePosY} L ${endNodePosX} ${endNodePosY}`;
-        let dStroke = "red";
-        let dStrokeWidth = "1";
-        let dFill = "none";
-
-        const path = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "path"
-        );
-        path.classList.add("path-between-nodes");
-        path.setAttribute("d", dPath);
-        // path.setAttribute("stroke", dStroke);
-        // path.setAttribute("stroke-width", dStrokeWidth);
-        // path.setAttribute("fill", dFill);
-        path.setAttribute("data-from", document.getElementById(node).id);
-        path.setAttribute("data-to", document.getElementById(neighbor).id);
-
-        network.appendChild(path);
-    });
-});
-
 /**
  * Implementation of the dijkstra algorithm. Finds the shortest path between two points
  *
- * @param {Map} graph Represents the network made out of nodes. Graph should include all of the edge informations. Format: {Point: {Point: Cost, (...)}, (...)}.
+ * @param {Object{Array, Array}} graph Represents the network made out of nodes. Graph should include all of the edge informations.
  * @param {String} start Starting point of the algorithm.
  * @param {String} end Ending point of the algorithm.
  */
-const findShortestPath = (graph, start, end) => {
-    //FIXME: If two edges have the same weight, the algorithm does not always pick the real shortest path, and does not go back to check so.
-    let path = [];
-    let queue = [];
-    let prev = new Map();
-    let distances = new Map();
+function dijkstra(graph, startNodeId, endNodeId) {
+    // Initialize letiables
+    let unvisitedNodes = new PriorityQueue();
+    let distance = {};
+    let previous = {};
 
-    graph.forEach((value, key) => {
-        distances.set(key, Infinity);
-        prev.set(key, null);
-        queue.push(key);
+    // Set initial conditions
+    graph.nodes.forEach(function (d) {
+        distance[d.id] = Infinity;
+        previous[d.id] = null;
+        unvisitedNodes.enqueue(d.id, distance[d.id]);
     });
+    distance[startNodeId] = 0;
+    unvisitedNodes.updatePriority(startNodeId, 0);
 
-    distances.set(start, 0);
+    let traversalArray = [];
 
-    let currentNode;
-    let currentNeighbors = {};
-    let currentMinDistance = 0;
+    // Main loop
+    while (unvisitedNodes.length > 0) {
+        // Find the unvisited node with the smallest distance
+        let currentNodeId = unvisitedNodes.dequeue();
+        traversalArray.push(currentNodeId);
 
-    while (queue.length > 0) {
-        let currentDistances = [];
-
-        distances.forEach((distance, key) => {
-            if (queue.includes(key)) {
-                currentDistances.push(distances.get(key));
-                currentMinDistance = Math.min(...currentDistances);
-                if (distance === currentMinDistance) {
-                    currentNode = key;
-                    return;
-                }
-            }
-        });
-
-        if (currentNode === end) {
-            path.push(currentNode);
-            let result = [];
-
-            while(prev.get(currentNode) != null) {
-                result.push(currentNode);
-                currentNode = prev.get(currentNode);
-            }
-            result.push(start);
-
-            return result.reverse();
+        // Stop if we've reached the end node
+        if (currentNodeId === endNodeId) {
+            break;
         }
 
-        path.push(currentNode);
-
-        queue = queue.filter((value) => {
-            return value !== currentNode;
-        });
-
-        currentNeighbors = graph.get(currentNode);
-
-        currentNeighbors.forEach((weight, neighbor) => {
-            if (queue.includes(neighbor)) {
-                if (distances.get(currentNode) + weight < distances.get(neighbor)) {
-                    distances.set(neighbor, distances.get(currentNode) + weight);
-                    prev.set(neighbor, currentNode)
+        // Update the distance of each neighbor
+        graph.links
+            .filter(function (d) {
+                return (
+                    d.source.id === currentNodeId ||
+                    d.target.id === currentNodeId
+                );
+            })
+            .forEach(function (link) {
+                let neighborId =
+                    link.target.id === currentNodeId
+                        ? link.source.id
+                        : link.target.id;
+                let newDistance = distance[currentNodeId] + link.weight;
+                if (newDistance < distance[neighborId]) {
+                    distance[neighborId] = newDistance;
+                    previous[neighborId] = currentNodeId;
+                    unvisitedNodes.updatePriority(neighborId, newDistance);
                 }
-            }
-        });
+            });
     }
-};
 
-export {graph, findShortestPath};
+    // Build the path
+    let path = [];
+    let currentNode = endNodeId;
+    while (currentNode !== null) {
+        path.unshift(currentNode);
+        currentNode = previous[currentNode];
+    }
+
+    return {
+        path: path,
+        traversalArray,
+        distance: distance[endNodeId],
+    };
+}
+
+//PriorityQueue
+class PriorityQueue {
+    constructor() {
+        this.queue = [];
+        this.priorities = {};
+    }
+
+    enqueue(value, priority) {
+        this.queue.push(value);
+        this.priorities[value] = priority;
+    }
+
+    dequeue() {
+        let highestPriority = Infinity;
+        let highestPriorityIndex = null;
+
+        for (let i = 0; i < this.queue.length; i++) {
+            if (this.priorities[this.queue[i]] < highestPriority) {
+                highestPriority = this.priorities[this.queue[i]];
+                highestPriorityIndex = i;
+            }
+        }
+
+        let dequeued = this.queue.splice(highestPriorityIndex, 1);
+        return dequeued[0];
+    }
+
+    updatePriority(value, priority) {
+        this.priorities[value] = priority;
+    }
+    get length() {
+        return this.queue.length;
+    }
+}
+
+export default dijkstra;
